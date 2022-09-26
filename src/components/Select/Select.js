8690fb792444ce84b166e19cc7f3c93969053c8d
Adnan Haque <eAdnan007@users.noreply.github.com>
2022-09-26 12:15:02 +0000

    Implement Select

--- src/components/Select/Select.js
+++ src/components/Select/Select.js
@@ -5,13 +5,44 @@ import { COLORS } from '../../constants';
 import Icon from '../Icon';
 import { getDisplayedValue } from './Select.helpers';
 
+const Wrapper = styled.div`
+  position: relative;
+  width: max-content;
+  color: ${COLORS.gray700};
+  background-color: ${COLORS.transparentGray15};
+  border-radius: 8px;
+  padding: 12px 52px 16px 12px;
+
+  select {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    opacity: 0;
+  }
+`;
+
+const DropDownIcon = styled(Icon)`
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  margin: auto;
+  right: 10px;
+}
+`;
+
 const Select = ({ label, value, onChange, children }) => {
   const displayedValue = getDisplayedValue(value, children);
 
   return (
-    <select value={value} onChange={onChange}>
-      {children}
-    </select>
+    <Wrapper>
+      {displayedValue}
+      <DropDownIcon id="chevron-down" strokeWidth={1} size={24} />
+      <select value={value} onChange={onChange}>
+        {children}
+      </select>
+    </Wrapper>
   );
 };
 
